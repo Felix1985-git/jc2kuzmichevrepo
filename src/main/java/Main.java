ab8b8d690613817bbb0c4bcf1cf72399a76841c8
Kuzmichou Aliaksei <kuzmichoualiaksei@gmail.com>
2021-04-08 20:46:15 +0300

    Dev commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello 2");
-        System.out.println("cherry-pick");
-        System.out.println("Master final");
     }
 }
